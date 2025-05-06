debbbd40a3d4bb952a751a0cc3255948447703d5
jannathsyeda <jannathsyeda7@gmail.com>
2025-05-06 13:31:54 +0600

    recipes done

--- src/Photo.jsx
+++ src/Photo.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import React from "react";
 
-import { getImageUrl } from './assets/utils';
-import { people } from './assets/data';
-
+import { getImageUrl } from "./assets/utils";
+import { people } from "./assets/data";
+import { recipes } from "./assets/data";
 export default function Photo() {
-    const listItems=people.map(person=>{
-        <li key={person.id}>
-            <img
-            src={getImageUrl(person)} 
-            />
-            <b>{person.name}:</b>
-            
-        </li>
-    })
+  const listItems = people.map((person) => (
+    <li key={person.id}>
+      <img src={getImageUrl(person)} />
+      <b>{person.name}:</b>
+      <b>person</b>
+    </li>
+  ));
 
-        return (
-        
-        )
+  const everyoneElse = people.filter(
+    (person) => person.profession !== "chemist"
+  );
+
+  const everyoneElseMap = everyoneElse.map((person) => (
+    <li key={person.id}>
+      <img src={getImageUrl(person)} />
+      <b>{person.name}:</b>
+    </li>
+  ));
+
+  return (
+    <div>
+      <div className="flex">
+        <ul>
+          <h6> only chemists</h6>
+          {listItems}
+        </ul>
+
+        <ul>
+          <h1>Every one else</h1>
+          {everyoneElseMap}
+        </ul>
+      </div>
+
+      <h1> Recipies section</h1>
+      <div>
+        {
+            recipes.map(recipe=>
+                <div key={recipe.id}>
+                    <h1>{recipe.name}</h1>
+                    <ul>
+                {recipe.ingredients.map(ingredient=>
+                <li key={ingredient}>{ingredient}</li>
+
+                )}
+                    </ul>
+
+                </div>
+
+            )
+        }
+
+      </div>
+    </div>
+  );
 }
